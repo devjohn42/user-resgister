eaa80dccbeafe8b2b305ae62cc4b5dbc66bb8865
João Vitor <devjohncb@gmail.com>
2023-07-04 20:58:51 -0300

    creating the header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
 import Header from "./components/global/Header";
 import Main from "./components/Main";
 import Footer from "./components/global/Footer";
 
 function App() {
   return (
-    <div className="h-[100vh] flex flex-col items-center justify-center">
-      <h1 className="text-principal text-9xl text-center font-bold">
-        DS Levelup!!
-      </h1>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="h-[100vh] bg-main">
+        {/* <h1 className="text-principal text-9xl text-center font-bold red">
+          DS Levelup!!
+        </h1> */}
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import logo from "/DSlevelup.png";
+
+const link =
+  "relative text-principal font-semibold text-2xl  duration-300 hover:ease-in hover:duration-300 noHover yesHover";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="w-full bg-content py-5 flex items-center justify-around drop-shadow-[0_1px_4px_rgba(0,0,0,0.6)]">
+      <img src={logo} alt="" className="w-[230px]" />
+      <nav className="relative flex gap-4">
+        <Link to="/" className={link}>
+          Home
+        </Link>
+        <Link to="/register" className={link}>
+          Register
+        </Link>
+        <Link to="/users" className={link}>
+          Users
+        </Link>
+      </nav>
+    </header>
+  );
 }
