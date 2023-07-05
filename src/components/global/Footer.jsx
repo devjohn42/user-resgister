3942933e63082488848f64535ce8f3cda538f9fc
João Vitor <devjohncb@gmail.com>
2023-07-04 21:24:02 -0300

    making some changes to the header component and creating the footer component

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -1,5 +1,34 @@
 import React from "react";
 
+import { FaLinkedin, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+
+import discord from "/discord.svg";
+
+const link = "text-3xl text-principal";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="absolute bottom-0 w-full bg-content py-5 flex items-center justify-around drop-shadow-[0_1px_4px_rgba(0,0,0,0.6)]">
+      <div className="flex gap-8">
+        <a href="#" className={link}>
+          <FaLinkedin />
+        </a>
+        <a href="#" className={link}>
+          <FaGithub />
+        </a>
+        <a href="#" className={link}>
+          <FaInstagram />
+        </a>
+        <a href="#" className={link}>
+          <FaTwitter />
+        </a>
+      </div>
+      <h1 className="text-principal font-primary font-bold text-xl">
+        Developed by DevJohn
+      </h1>
+      <a href="https://discord.com/" target="blank">
+        <img src={discord} alt="" className="w-[170px]" />
+      </a>
+    </footer>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily: {},
+    fontFamily: {
+      primary: "Roboto"
+    },
     screens: {
       sm: "640px",
       // => @media (min-width: 640px)
