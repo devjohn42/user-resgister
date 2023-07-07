5015cb9d42b4c493f381587f555ae44a706d08d8
João Vitor <devjohncb@gmail.com>
2023-07-06 23:58:59 -0300

    starting the register component logic

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const input = `bg-content py-[0.5rem] px-[1rem] rounded-[0.5rem] outline-none 
   drop-shadow-[0_0px_2px_rgba(0,0,0,0.6)] font-normal text-lg font-primary 
   text-principal placeholder-white/30`;
 
+const baseURL = "http://localhost:3000/users";
+
 export default function Register() {
+  const [data, setData] = useState({
+    user: { name: "", email: "", nick: "" },
+    list: [],
+  });
+
+  const handleSave = () => {
+    const user = data.user;
+    const method = user.id ? "put" : "post";
+    const URL = user.id ? `${baseURL}/${user.id}` : baseURL;
+    axios[method](URL, user).then((resp) => {
+      const list = handleUpdateList(resp.data);
+      setData({ user: data.user, list });
+    });
+  };
+
+  const handleUpdateList = (user, add = true) => {
+    const list = data.list.filter((u) => u.id !== user.id);
+    if (add) list.unshift(user);
+    return list;
+  };
+  console.log(data.user.email, data.user.name, data.user.nick);
   return (
     <main className="h-full flex flex-col items-center justify-center">
       <div
@@ -39,6 +63,7 @@ export default function Register() {
             className="bg-principal/70 py-1 px-8 font-primary text-lg font-bold 
           text-white rounded-[0.5rem] duration-300 
           hover:bg-principal hover:ease-in hover:duration-300"
+            onClick={(e) => handleSave(e)}
           >
             Registrar
           </button>
